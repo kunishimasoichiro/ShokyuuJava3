6ba6667ccfaf6dd1ebae243ae84d06793729522f
國島聡一郎 <kunishima0912@gmail.com>
2024-05-14 10:46:36 +0900

    Java3.1の追加

--- 初級Java2.5/src/start.java
+++ 初級Java2.5/src/start.java
@@ -2,17 +2,8 @@
 public class start {
 
 	public static void main(String[] args) {
-		MyFrame frame1=new MyFrame();
-        frame1.setLocation(200,100);
-        frame1.setSize(150,150);
-        MyFrame frame2=new MyFrame();
-        frame2.setLocation(350,200);
-        MyFrame frame3=new RectFrame();
-        frame3.setLocation(50,300);
-        
-        int x = 2;
-        int y = 3;
-        
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	}
 
 }
