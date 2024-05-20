35f3547c765df26a4d25eae2cd17d90dd5556716
國島聡一郎 <kunishima0912@gmail.com>
2024-05-20 12:09:42 +0900

    Java4.3の追加

---
+++ 初級Java2.5/src/RectIf.java
@@ -0,0 +1,17 @@
+
+public class RectIf extends MyFrame {
+	public void run() {
+		int x=30;
+		setColor(0,128,0);
+		int i;  // 繰り返しに使う変数iを宣言
+		for(i=0 ; i<9 ; i++) { // i=0から、i<9の間繰り返す。繰り返すたびにiを1増やす
+			if(i>=4 && i<=7) {
+				fillRect(x,100,10,100);
+			}else {
+				fillRect(x,80,10,100);
+			}
+			x+=20;
+		}
+	}
+
+}
