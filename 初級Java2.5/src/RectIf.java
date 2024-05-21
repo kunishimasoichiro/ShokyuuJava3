6a1bfb5dc628b83b365c065d75bacc91e8096a3d
國島聡一郎 <kunishima0912@gmail.com>
2024-05-21 11:50:36 +0900

    おまけ1

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -2,6 +2,7 @@
 public class RectIf extends MyFrame {
 	public void run() {
 		int x=30;
+<<<<<<< Updated upstream
 		setColor(0,128,0);
 		int i;  // 繰り返しに使う変数iを宣言
 		for(i=0 ; i<9 ; i++) { // i=0から、i<9の間繰り返す。繰り返すたびにiを1増やす
@@ -11,6 +12,15 @@ public class RectIf extends MyFrame {
 				fillRect(x,80,10,100);
 			}
 			x+=20;
+=======
+		int y=20;
+		setColor(0,128,0);
+		int i;  // 繰り返しに使う変数iを宣言
+		for(i=0 ; i<9 ; i++) {
+			fillRect(x,x,y,y);
+			x+=y;
+			y+=10;
+>>>>>>> Stashed changes
 		}
 	}
 
