7b4aeb8d280e07338cd5a18e3cf1ea784e3c9969
國島聡一郎 <kunishima0912@gmail.com>
2024-05-28 10:06:04 +0900

    Java6.2の追加

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,12 +1,12 @@
 
 public class DisplayFaceObject extends MyFrame {
 	public void run() {
-		Face face1=new Face();
+		Face face1=new Face(50,50);
 		face1.x=100;
 		face1.y=50;
 		face1.draw(this);
 		
-		Face face2=new Face();
+		Face face2=new Face(200,100);
 		face2.x=200;
 		face2.y=150;
 		face2.draw(this);
